dedca504424c5acef3cafc5ccacc1ad5fe587793
aboelmagd2004 <gtg90903@gmail.com>
2025-05-09 15:38:13 +0300

    Delete TechXpress-master/TechXpress.Data directory

--- TechXpress-master/TechXpress.Data/Models/CreateIntentRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TechXpress.Web.Models
-{
-    public class CreateIntentRequest
-    {
-        public long Amount { get; set; }
-        public string Currency { get; set; }
-        public int OrderId { get; set; }
-    }
-}
